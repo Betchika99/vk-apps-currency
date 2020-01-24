7554aee3da853c3a228c883163f15dc5df602bc6
Elizabeth Dobryanskaya <dobrl2011@mail.ru>
2020-01-25 02:45:58 +0300

    Updates

---
+++ precache-manifest.f73150251c7e5a8b1104accc856476b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cf9f704a32f29bb47354505980508d",
+    "url": "/vk-apps-currency/index.html"
+  },
+  {
+    "revision": "0f77d8f8564eeb26053a",
+    "url": "/vk-apps-currency/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "a8e1007831ba73d92720",
+    "url": "/vk-apps-currency/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0f77d8f8564eeb26053a",
+    "url": "/vk-apps-currency/static/js/2.ff1466b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vk-apps-currency/static/js/2.ff1466b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8e1007831ba73d92720",
+    "url": "/vk-apps-currency/static/js/main.d0141fa1.chunk.js"
+  },
+  {
+    "revision": "dae897d31523e3918d19",
+    "url": "/vk-apps-currency/static/js/runtime-main.2c852996.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  }
+]);
